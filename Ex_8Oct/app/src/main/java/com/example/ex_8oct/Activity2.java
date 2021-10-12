35ce37d002a01c57a94bd1958f4a8e118a0c9f2d
ColonelAVP <athervpatil05@gmail.com>
2021-10-12 14:10:02 +0530

    updated Ex

--- Ex_8Oct/app/src/main/java/com/example/ex_8oct/Activity2.java
+++ Ex_8Oct/app/src/main/java/com/example/ex_8oct/Activity2.java
@@ -2,13 +2,31 @@ package com.example.ex_8oct;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Activity2 extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+        Button bb = (Button)findViewById(R.id.b1);
+        bb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent data = new Intent();
+                EditText txt = (EditText)findViewById(R.id.ed);
+                data.setData(Uri.parse(txt.getText().toString()));
+                setResult(RESULT_OK, data);
+                finish();
+
+            }
+        });
     }
 }
--- Ex_8Oct/app/src/main/java/com/example/ex_8oct/MainActivity.java
+++ Ex_8Oct/app/src/main/java/com/example/ex_8oct/MainActivity.java
@@ -7,9 +7,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private final static int REQUEST_CODE_1 = 1;
+    int request_Value =1;
 
 
     @Override
@@ -24,7 +26,17 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(i, REQUEST_CODE_1);
 
             }
-        });
+        });}
 
+    @Override
+    protected void onActivityResult(int requestCode, int ResultCode, Intent data) {
+        super.onActivityResult(requestCode, ResultCode, data);
+        if (requestCode == request_Value)
+            if (ResultCode == RESULT_OK) {
+                Toast.makeText(this, data.getData().toString(), Toast.LENGTH_LONG).show();
+
+            }
     }
+
+
 }
